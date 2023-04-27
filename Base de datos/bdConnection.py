5a9f5285a0ea600ffbb597b741768dffbf441ca1
AlbertM <albertmunoz131@gmail.com>
2023-04-26 21:25:03 -0500

    Corrección BD V3

--- Base de datos/bdConnection.py
+++ Base de datos/bdConnection.py
@@ -13,6 +13,7 @@ def GuardarConexion(data):
     cur.execute(data)
     conexion.commit()
 
+
 GuardarConexion("CREATE TABLE SignosVitales (id SERIAL PRIMARY KEY, Nombre_signo VARCHAR(50), Unidad VARCHAR(10))")
 GuardarConexion("CREATE TABLE Diagnostico (id SERIAL PRIMARY KEY, Nombre_diagnostico VARCHAR(100), Descripcion VARCHAR(200))")
 GuardarConexion("CREATE TABLE Usuario (id SERIAL PRIMARY KEY, Nombre VARCHAR(100), Apellido VARCHAR(200), Cedula INT, Edad INT, Telefono VARCHAR(10), Email VARCHAR(100), Password VARCHAR(50), Direccion VARCHAR(100))")
@@ -23,3 +24,4 @@ GuardarConexion("CREATE TABLE personalMedico (id SERIAL PRIMARY KEY, usuario_id 
 GuardarConexion("CREATE TABLE HistorialDiagnostico (id SERIAL PRIMARY KEY, medico_id INT, Fecha TIMESTAMP, Paciente_id INT, Diagnostico_id INT, CONSTRAINT fkMedicoHDiagnostico FOREIGN KEY (medico_id) REFERENCES PersonalMedico(id), CONSTRAINT fkPacienteHDiagnostico FOREIGN KEY (paciente_id) REFERENCES Paciente(id), CONSTRAINT fkDiagHDiagnostico FOREIGN KEY (diagnostico_id) REFERENCES Diagnostico(id))")
 GuardarConexion("CREATE TABLE HistorialCuidados (id SERIAL PRIMARY KEY, Fecha_Inicial TIMESTAMP, Fecha_Final TIMESTAMP, Cuidado VARCHAR(100),  medico_id INT, Paciente_id INT, Descripcion VARCHAR(1000), CONSTRAINT fkHCuidadosPaciente FOREIGN KEY (paciente_id) REFERENCES Paciente(id), CONSTRAINT fkHCuidadosMedico FOREIGN KEY (medico_id) REFERENCES PersonalMedico(id))")
 GuardarConexion("CREATE TABLE HistorialSignoVital (id SERIAL PRIMARY KEY, Fecha TIMESTAMP, Valor FLOAT, signo_id INT, Paciente_id INT, CONSTRAINT fkHSignoPaciente FOREIGN KEY (paciente_id) REFERENCES Paciente(id), CONSTRAINT fkHSignosSigno FOREIGN KEY (signo_id) REFERENCES SignosVitales(id))")
+GuardarConexion("CREATE TABLE PersonalACargo(id SERIAL PRIMARY KEY, medico_id INT, Paciente_id INT, Paciente_activo boolean, CONSTRAINT fkPersonalMedico FOREIGN KEY (medico_id) REFERENCES PersonalMedico(id), CONSTRAINT fkPacientePersonal FOREIGN KEY (paciente_id) REFERENCES Paciente(id) )")
